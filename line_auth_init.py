7d0d4c0a750b6744282c0ac1385c655a65221556
zein12 <jena.zein@gmail.com>
2017-02-26 06:08:29 +0700

    Update line_auth_init.py

--- line_auth_init.py
+++ line_auth_init.py
@@ -6,8 +6,8 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 
-CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET', None)
+CHANNEL_ACCESS_TOKEN = os.getenv('3/cEBpOR0mjAMUtnHKrSrx3N6FnMVNPYfXBIwMO6HNGaljxuxTxZz2fGrmZYFwqfV3dvAWMa7FEGrmOONfbZ7or1wxYgpjbtFMS0Mkk+RftjvYSrUpThxAHGiivf2M662z2zM5P8BSKby0dJiBG3GQdB04t89/1O/w1cDnyilFU=', None)
+CHANNEL_SECRET = os.getenv(' a6b4b1a80d9f25eb0a719fc92cef7d86 ', None)
 
 if CHANNEL_ACCESS_TOKEN is None:
     print('(Error) Need to specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
